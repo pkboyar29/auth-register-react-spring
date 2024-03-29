d13b610ceaf7414e1967571079d826f84e97c14c
Eduard Musatov <pkboyar09@gmail.com>
2024-03-29 10:33:13 +0300

    Added spring boot configuration, connected postresql using docker container of postgresql

---
+++ backend/src/main/java/com/example/backend/Repositories/UserRepository.java
@@ -0,0 +1,10 @@
+package com.example.backend.Repositories;
+
+import com.example.backend.Models.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface UserRepository extends JpaRepository<User, Long> {
+
+}
