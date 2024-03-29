fb4873a14381160b2b16ee7a31acc441de015643
Eduard Musatov <pkboyar09@gmail.com>
2024-03-29 23:51:46 +0300

    Model fixed

--- backend/src/main/java/com/example/backend/models/User.java
+++ backend/src/main/java/com/example/backend/models/User.java
@@ -6,40 +6,43 @@ import lombok.Setter;
 import java.time.LocalDateTime;
 
 @Entity
-@Table(name="Users")
+@Table(name="users")
 @Getter
 @Setter
 public class User {
 
-    private enum AGE_LIMIT {
-        under18,
-        over18
-    }
-    private enum GENDER {
-        male,
-        female
-    }
-    private enum THEME {
-        light,
-        dark
-    }
-
     @Id
-    @Column(name = "id", unique = true, updatable = false)
-    private String id;
-    @Column(name = "username", unique = true)
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id", updatable = false)
+    private Long id;
+
+    @Column(name = "username", unique = true, nullable = false)
     private String username;
+
+    @Column(name = "first_name", nullable = false)
     private String firstName;
+
+    @Column(name = "last_name", nullable = false)
     private String lastName;
-    @Column(name = "email", unique = true)
+
+    @Column(name = "email", unique = true, nullable = false)
     private String email;
+
+    @Column(name = "password", nullable = false)
     private String password;
-    @Enumerated(EnumType.STRING)
-    private AGE_LIMIT ageLimit;
-    @Enumerated(EnumType.STRING)
-    private GENDER gender;
+
+    @Column(name = "age_limit", nullable = false)
+    private String ageLimit;
+
+    @Column(name = "gender", nullable = false)
+    private String gender;
+
+    @Column(name = "accept_rules", nullable = false)
     private Boolean acceptRules;
-    @Enumerated(EnumType.STRING)
-    private THEME theme;
+
+    @Column(name = "theme", nullable = false)
+    private String theme;
+
+    @Column(name = "created", columnDefinition = "TIMESTAMP DEFAULT CURRENT_TIMESTAMP")
     private LocalDateTime created;
 }
